cc34b89d1ec8df9136a5e7531abd2a1738802fc1
Neil Wallace <rationalise@hotmail.com>
2011-11-07 23:17:10 +0000

    files

---
+++ src/KaliedoApp.cpp
@@ -0,0 +1,208 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/Rand.h"
+#include "SoundEngine.h"
+
+using namespace ci;
+using namespace ci::app;
+
+#include <list>
+using namespace std;
+
+const float frame_time = 1.0f / 60.0f;
+
+class CLayer
+{
+public:
+	static CLayer* CreateRandom()
+	{
+		int num_divs = Rand::randInt(2, 20);
+		float rotate_rate = Rand::randFloat(-20, 20);
+		float inner_rotate = Rand::randFloat(-2, 2);
+		float radius = Rand::randFloat(0.1f, 2);
+		float life_time = Rand::randFloat(1.5f, 3.0f);
+
+		return new CLayer(num_divs, rotate_rate, inner_rotate, radius, life_time);
+	}
+
+	bool Update(float delta_time)
+	{
+		m_CurrLife -= delta_time;
+
+		if(m_CurrLife > 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	void draw(float time, float w, float h)
+	{
+		glPushMatrix();
+
+		float life_percent = math<float>::clamp(m_CurrLife / m_TotalLife);
+		float inv_life_percent = 1 - life_percent;
+		float in_out = ((float)sin(inv_life_percent * M_PI * 2) + 1) * 0.5f;
+
+		gl::rotate(time * m_MasterRotateRate);
+
+		int num_divs = 10;
+		for(int i=0; i<num_divs; i++)
+		{
+			float i_f = i/(float)num_divs;
+			gl::color(hsvToRGB(Vec3f(i_f, 1, 1)));
+
+			float sin_t = sin(time*1.3f);
+
+			glPushMatrix();
+
+			gl::rotate(i_f*360);
+			gl::translate(0, w * 0.25f * m_Radius);
+
+			float rect_w = 10;
+			float rect_h = 10;
+
+			float cube_size = 20;
+			gl::rotate(Vec3f(time*18*m_InnerRotateRate, time*36*m_InnerRotateRate, 0));
+
+			gl::scale(in_out, in_out, in_out);
+
+			gl::drawCube(Vec3f::zero(), Vec3f(1,1,1) * (cube_size * (sin_t+2)));
+
+			glPopMatrix();
+		}
+
+		glPopMatrix();
+	}
+
+private:
+	CLayer(int num_divs, float rotate_rate, float inner_rotate, float radius, float life_time)
+		:
+	m_NumDivs(num_divs),
+		m_MasterRotateRate(rotate_rate),
+		m_InnerRotateRate(inner_rotate),
+		m_Radius(radius),
+		m_TotalLife(life_time),
+		m_CurrLife(life_time)
+	{
+
+	}
+
+	int		m_NumDivs;
+
+	float m_MasterRotateRate;
+	float m_InnerRotateRate;
+	float m_Radius;
+
+	float m_TotalLife;
+	float m_CurrLife;
+};
+
+class BasicApp : public AppBasic 
+{
+public:
+	void setup();
+	void update();
+	void draw();
+
+	std::list<CLayer*> m_Layers;
+	CSoundEngine* mp_SoundEngine;
+};
+
+void BasicApp::setup()
+{
+	//	gl::enableDepthWrite();
+	//	gl::enableDepthRead();
+
+	glCullFace(GL_FRONT_FACE);
+	glEnable(GL_CULL_FACE);
+	gl::enableAlphaBlending();
+
+	setFrameRate(60);
+
+	for(int i=0; i<10; ++i)
+	{
+		m_Layers.push_back(CLayer::CreateRandom());
+	}
+
+	CSoundEngine::Create(this);
+}
+
+void BasicApp::update()
+{
+	if(CSoundEngine::IsValid())
+	{
+		CSoundEngine::Get().Update();
+	}
+
+	int insert_count = 0;
+
+	for(std::list<CLayer*>::iterator it = m_Layers.begin(); it != m_Layers.end();)
+	{
+		if((*it)->Update(frame_time))
+		{
+			++it;
+		}
+		else
+		{
+			it = m_Layers.erase(it);
+			insert_count++;
+		}
+	}
+
+	for(int i=0; i<insert_count; ++i)
+	{
+		m_Layers.push_back(CLayer::CreateRandom());
+	}
+}
+
+void BasicApp::draw()
+{
+	glEnable(GL_CULL_FACE);
+
+	static float time = 0;
+	time += frame_time;
+
+	float w = (float)getWindowWidth();
+	float h = (float)getWindowHeight();
+
+	float w_2 = w * 0.5f;
+	float h_2 = h * 0.5f;
+
+	gl::clear(ColorA::black(), true);
+
+	glPushMatrix();
+	gl::translate(w_2, h_2);
+
+	for(std::list<CLayer*>::iterator it = m_Layers.begin(); it != m_Layers.end(); ++it)
+	{
+		//(*it)->draw(time, w, h);
+	}
+
+	gl::translate(-w_2, -h_2);
+
+	glPopMatrix();
+
+
+	glDisable(GL_CULL_FACE);
+	int num_divs = 10;
+
+	float blob_gap = w/(num_divs*2.0f);
+	float blob_radius = blob_gap * 0.5f;
+
+	for(int i=0; i<num_divs; i++)
+	{
+		float i_f = i/(float)num_divs;
+
+		gl::color(hsvToRGB(Vec3f(i_f, 1, 1)));
+		float val = CSoundEngine::Get().GetAverage(i_f);
+		gl::drawSolidCircle(Vec2f(w * i_f + blob_gap, h-blob_gap), val * blob_radius);
+
+		gl::color(hsvToRGB(Vec3f(1-i_f, 1, 0.5f)));
+		val = CSoundEngine::Get().GetShortAverage(i_f);
+		gl::drawSolidCircle(Vec2f(w * i_f + blob_gap, h-blob_gap), val * blob_radius);
+	}
+}
+
+// This line tells Flint to actually create the application
+CINDER_APP_BASIC( BasicApp, RendererGl )
---
+++ xcode/SoundEngine.h
@@ -0,0 +1,103 @@
+#pragma once
+#include "cinder/audio/Output.h"
+#include "cinder/audio/Io.h"
+#include "Kiss.h"
+#include "cinder/app/AppBasic.h"
+
+#include <boost/filesystem.hpp>
+
+
+#include <list>
+
+#define NUM_BARK_SCALE_BANDS 25
+#define NUM_FFT_BANDS 512
+
+enum ENormalizationStrategy
+{
+	NORMALIZE_CLAMP_BARK_BANDS,
+	SMOOTH_LIMIT_BARK_BANDS,
+};
+
+//--------------------------------------------------------------------------------------
+class CSoundEngine
+{
+public:
+	CSoundEngine(ci::app::AppBasic* p_parent) 
+		:
+	mp_App(p_parent)
+	{ 
+		Init(); 
+	}
+
+	~CSoundEngine() { Shutdown(); }
+
+
+	void		Init();
+	void		InitBarkVals();
+	void		Update();
+	void		ProcessAudio(float time_step);
+	void		NormalizeAudio();
+	void		Draw();
+	
+	void		NextFile();
+
+	float		GetAmplitude(float f);
+	float		GetAverage(float f);
+	float		GetShortAverage(float f);
+	float		GetMovement(float f);
+
+	float		GetSmoothVolume() const { return m_SmoothVolume; }
+
+	float*		GetRawAudio(int& num);
+
+	static CSoundEngine& Get() { return *mp_Impl; }
+	static void Create(ci::app::AppBasic* p_parent) { mp_Impl = new CSoundEngine(p_parent); }
+	static void Destroy() { if(mp_Impl != NULL) { mp_Impl->Shutdown(); delete mp_Impl; mp_Impl = NULL; } }
+	static bool IsValid() { return mp_Impl != NULL; }
+	
+	// Analyzer
+	bool mFftInit;
+	Kiss mFft;
+
+
+//private:
+	void		Shutdown();
+
+	float		GetVariance(float f);
+	float		GetValue(float* vals, float f);
+
+	ci::app::AppBasic* mp_App;
+
+	// Audio file
+	audio::SourceRef mAudioSource;
+	audio::TrackRef mTrack;
+	audio::PcmBuffer32fRef mBuffer;
+	
+	std::vector<boost::filesystem::directory_entry> m_FileList;
+
+
+
+	string			m_CurrentFile;
+
+
+	float* mp_FFTOutput;
+
+	float 				m_BarkScaleValues[NUM_BARK_SCALE_BANDS];
+	float 				m_BarkShortAverages[NUM_BARK_SCALE_BANDS];
+	float 				m_BarkLongAverages[NUM_BARK_SCALE_BANDS];
+	float 				m_BarkVariance[NUM_BARK_SCALE_BANDS];
+	float 				m_BarkBandFrequencies[NUM_BARK_SCALE_BANDS];
+	float 				m_BarkBandMultipliers[NUM_BARK_SCALE_BANDS];
+
+	std::list<float>	m_History[NUM_BARK_SCALE_BANDS];
+
+	int* binToBark;
+
+	int m_BufferSize;
+	int m_NumFFTBands;
+
+	float m_SmoothVolume;
+	float m_SmoothBarkBandMax;
+
+	static CSoundEngine* mp_Impl;
+};
